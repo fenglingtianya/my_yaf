cde91f9389e25a980a314d2574784b58763d595f
xuelong01 <xuelong01@58ganji.com>
2017-07-03 20:31:37 +0800

    hh

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -3,6 +3,7 @@
  * @name Bootstrap
  * @author corp\xuelong
  * @desc 所有在Bootstrap类中, 以_init开头的方法, 都会被Yaf调用,
+ * jdfijdijaidjfidjia
  * @see http://www.php.net/manual/en/class.yaf-bootstrap-abstract.php
  * 这些方法, 都接受一个参数:Yaf_Dispatcher $dispatcher
  * 调用的次序, 和申明的次序相同
